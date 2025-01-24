0c4496b4b15c8adbb3bee40a56e0731055c2b29f
Saud Saleem <saudsaleem@Sauds-MacBook-Air.local>
2025-01-25 02:21:59 +0500

    fix

--- finetuning.py
+++ finetuning.py
@@ -92,7 +92,7 @@ for name, param in model.named_parameters():
 print('LORA RELATED PRINTS end')
 
 for name, param in model.named_parameters():
-    print(f"Layer: {name}, requires_grad: {param.requires_grad}")
+    print(f"Layer: {name}, param {param}, requires_grad: {param.requires_grad}")
     if 'q_proj' in name or 'k_proj' in name or 'v_proj' in name:
         print(f"LoRA Layer {name}: requires_grad={param.requires_grad}")
 
