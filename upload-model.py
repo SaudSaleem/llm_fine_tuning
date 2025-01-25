6a9c95306208cde0d30200d344a6b65871bdad5a
Saud Saleem <saudsaleem@Sauds-MacBook-Air.local>
2025-01-26 00:00:04 +0500

    changes

--- upload-model.py
+++ upload-model.py
@@ -9,20 +9,13 @@ model_name = model_path.split("/")[-1]  # Extract the model name from the model 
 hf_token = os.getenv("HF_TOKEN")
 print('hf_token', hf_token)
 login(token=hf_token)  # Replace with your Hugging Face access token
-
-# Initialize the API to create a new repository
 api = HfApi()
-
-# Create a new repo with the name derived from the model path
-repo_url = api.create_repo(repo_id=model_name, private=False)  # Private repo, you can change to False if public
-
+repo_url = api.create_repo(repo_id=model_name, private=False)
 # Initialize the repository
 repo = Repository(local_dir=model_path, clone_from=repo_url)
-
 # Add all files and commit them
 repo.git_add()
 repo.git_commit("Upload fine-tuned model")
-
 # Push the changes to Hugging Face
 repo.push_to_hub()
 
