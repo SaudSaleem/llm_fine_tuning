39ba8c2532543260575fdad866b3eeff145b060b
Saud Saleem <saudsaleem@Sauds-MacBook-Air.local>
2025-01-25 12:30:11 +0500

    fix

--- finetuning.py
+++ finetuning.py
@@ -28,7 +28,7 @@ print("Cuda version: ", torch.version.cuda)  # Prints the CUDA version that PyTo
 load_dotenv()
 
 # Load quantized model and tokenizer
-model_name = "thesven/Mistral-7B-Instruct-v0.3-GPTQ"
+model_name = "mistralai/Mistral-7B-Instruct-v0.3"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 # Set the padding token if it's not already defined
 if tokenizer.pad_token is None:
