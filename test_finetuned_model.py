1cdd316698ca1e8e8cb2a3296bcaacff7e623927
Saud Saleem <saudsaleem@Sauds-MacBook-Air.local>
2025-01-25 23:52:07 +0500

    changes

--- test_finetuned_model.py
+++ test_finetuned_model.py
@@ -5,7 +5,7 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # Use GPU ID 0, modify as needed
 # Check device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Device:", device)
+# print("Device:", device)
 
 def test_finetuned_model(model_path, prompt, max_length=100, num_beams=5):
     try:
@@ -15,11 +15,11 @@ def test_finetuned_model(model_path, prompt, max_length=100, num_beams=5):
         # Try moving the model to the GPU
         model = model.to(device)
 
-        # Check the model's device
-        if next(model.parameters()).device.type == "cuda":
-            print("The model is successfully moved to GPU.")
-        else:
-            print("The model is running on CPU.")
+        # # Check the model's device
+        # if next(model.parameters()).device.type == "cuda":
+        #     print("The model is successfully moved to GPU.")
+        # else:
+        #     print("The model is running on CPU.")
 
         # Tokenize input and move tensors to the same device
         inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=max_length)
@@ -30,7 +30,7 @@ def test_finetuned_model(model_path, prompt, max_length=100, num_beams=5):
         output = model.generate(
             input_ids=input_ids,
             attention_mask=attention_mask,
-            max_length=500,
+            max_length=200,
             num_beams=num_beams,
             pad_token_id=tokenizer.eos_token_id,
             no_repeat_ngram_size=2,
