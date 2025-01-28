85f0790efbc18de9d4f63cc0a7492bf69de10aea
Saud Saleem <saudsaleem@Sauds-MacBook-Air.local>
2025-01-29 00:56:22 +0500

    fixes

--- trainer.py
+++ trainer.py
@@ -94,7 +94,7 @@ bnb_config = BitsAndBytesConfig(
 
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
-    quantization_config=bnb_config,
+    # quantization_config=bnb_config,
     device_map="auto",
     attn_implementation="flash_attention_2"
 )
